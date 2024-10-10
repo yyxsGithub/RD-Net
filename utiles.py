117339f423e4b51e26a189f7684845e0e7c0896c
yyx <576571659@qq.com>
2024-10-10 21:17:38 +0800

    first commit

---
+++ utiles.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+data_path = "/datasets/shapenetcore_partanno_segmentation_benchmark_v0/"
+
+def array2samples_distance(array1, array2):
+    """
+    arguments: 
+        array1: the array, size: (num_point, num_feature)
+        array2: the samples, size: (num_point, num_feature)
+    returns:
+        distances: each entry is the distance from a sample to array1 
+    """
+    num_point1, num_features1 = array1.shape
+    num_point2, num_features2 = array2.shape
+    expanded_array1 = array1.repeat(num_point2, 1)
+    expanded_array2 = torch.reshape(
+            torch.unsqueeze(array2, 1).repeat(1, num_point1, 1),
+            (-1, num_features2))
+    distances = (expanded_array1-expanded_array2)* (expanded_array1-expanded_array2)
+#    distances = torch.sqrt(distances)
+    distances = torch.sum(distances,dim=1)
+    distances = torch.reshape(distances, (num_point2, num_point1))
+    distances = torch.min(distances,dim=1)[0]
+    distances = torch.mean(distances)
+    return distances
+
+def chamfer_distance_numpy(array1, array2):
+    batch_size, num_point, num_features = array1.shape
+    dist = 0
+    for i in range(batch_size):
+        av_dist1 = array2samples_distance(array1[i], array2[i])
+        av_dist2 = array2samples_distance(array2[i], array1[i])
+        dist = dist + (0.5*av_dist1+0.5*av_dist2)/batch_size
+    return dist*100
+
+def chamfer_distance_numpy_test(array1, array2):
+    batch_size, num_point, num_features = array1.shape
+    dist_all = 0
+    dist1 = 0
+    dist2 = 0
+    for i in range(batch_size):
+        av_dist1 = array2samples_distance(array1[i], array2[i])
+        av_dist2 = array2samples_distance(array2[i], array1[i])
+        dist_all = dist_all + (av_dist1+av_dist2)/batch_size
+        dist1 = dist1+av_dist1/batch_size
+        dist2 = dist2+av_dist2/batch_size
+    return dist_all, dist1, dist2
+
+class PointLoss(nn.Module):
+    def __init__(self):
+        super(PointLoss,self).__init__()
+        
+    def forward(self,array1,array2):
+        return chamfer_distance_numpy(array1,array2)
+
+class PointLoss_test(nn.Module):
+    def __init__(self):
+        super(PointLoss_test,self).__init__()
+        
+    def forward(self,array1,array2):
+        return chamfer_distance_numpy_test(array1,array2)
+
+def distance_squre(p1,p2):
+    tensor=torch.FloatTensor(p1)-torch.FloatTensor(p2)
+    val=tensor.mul(tensor)
+    val = val.sum()
+    return val
+
+def index_points(points, idx):
+    """
+    Input:
+        points: input points data, [B, N, C]
+        idx: sample index data, [B, S]
+    Return:
+        new_points:, indexed points data, [B, S, C]
+    """
+    device = points.device
+    B = points.shape[0]
+    view_shape = list(idx.shape)
+    view_shape[1:] = [1] * (len(view_shape) - 1)
+    repeat_shape = list(idx.shape)
+    repeat_shape[0] = 1
+    batch_indices = torch.arange(B, dtype=torch.long).to(device).view(view_shape).repeat(repeat_shape)
+    new_points = points[batch_indices, idx, :]
+    return new_points
+
+if __name__=='__main__':
+    a = torch.randn(64,256,3)
+    b = torch.randn(64,256,3)
+    c = torch.randn(64,64,3)
+    d = torch.randn(64,64,3)
+    p = PointLoss()
+    print(p(a,b))
+    print(p(c,d)*0.25)
